60c92d7f4f71c1ed28d910fe36d86a7819b35eae
slipknotbiocom <slipknotbiocom@gmail.com>
2019-03-05 22:54:09 +0300

    Get Json from AccuWeather
    Get json from accuweather if it not exist at /resources

--- WeatherNow/src/main/java/by/moiseenko/Runner.java
+++ WeatherNow/src/main/java/by/moiseenko/Runner.java
@@ -1,4 +1,18 @@
 package by.moiseenko;
 
+import by.moiseenko.utils.NetworkUtils;
+
+import java.io.File;
+
 public class Runner {
+  public static void main(String[] args) {
+    NetworkUtils networkUtils = new NetworkUtils();
+    File weatherJsonDir = new File("./resources/weather-forecast.json");
+    if (!weatherJsonDir.exists()) {
+      networkUtils.getContent(networkUtils.createConnection(networkUtils.buildURL()));
+      System.out.println("weather was uploaded completely");
+    } else {
+      System.out.println("weather-forecast.json EXIST");
+    }
+  }
 }
--- WeatherNow/src/main/java/by/moiseenko/utils/NetworkUtils.java
+++ WeatherNow/src/main/java/by/moiseenko/utils/NetworkUtils.java
@@ -1,20 +1,78 @@
 package by.moiseenko.utils;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.*;
+import java.net.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class NetworkUtils {
-    private static final String WEATHER_BASE_CODE = "28800";
-    private static final String API_KEY = "qxgnTnXdJgTgRGGNXHCOoAxDXazDQqqs";
-    private static final String PARAM_API_KEY = "apikey";
-//"http://dataservice.accuweather.com/forecasts/v1/daily/5day/28800?apikey=qxgnTnXdJgTgRGGNXHCOoAxDXazDQqqs&language=ru-Ru&details=true&metric=true"
-    public static URL buildURL() throws MalformedURLException {
+  private  static final Logger LOG = LogManager.getLogger(NetworkUtils.class);
+  private  final String WEATHER_BASE_CODE = "28800";
+  private  final String API_KEY = "qxgnTnXdJgTgRGGNXHCOoAxDXazDQqqs";
+  private  final String PARAM_API_KEY = "apikey";
+  private  final String assertResult =
+      "http://dataservice.accuweather.com/forecasts/v1/daily/5day/28800?apikey=qxgnTnXdJgTgRGGNXHCOoAxDXazDQqqs&language=ru-Ru&details=true&metric=true";
+  private  final int BUFFER_SIZE = 1024;
 
-        
+  public  URL buildURL() {
 
-        return  new URL("http://dataservice.accuweather.com/forecasts/v1/daily/5day/28800?apikey=qxgnTnXdJgTgRGGNXHCOoAxDXazDQqqs");
+    URL url = null;
+    try {
+      URI uri =
+          new URIBuilder()
+              .setScheme("http")
+              .setHost("dataservice.accuweather.com")
+              .setPath("/forecasts/v1/daily/5day/" + WEATHER_BASE_CODE)
+              .setParameter(PARAM_API_KEY, API_KEY)
+              .setParameter("language", "ru-Ru")
+              .setParameter("details", "true")
+              .setParameter("metric", "true")
+              .build();
+      url = uri.toURL();
+//      System.out.println(uri);
+//      System.out.println(uri.toString().equals(assertResult));
+    } catch (URISyntaxException | MalformedURLException e) {
+      LOG.error(e);
     }
+    return url;
+  }
 
+  public  HttpURLConnection createConnection(URL url) {
+    HttpURLConnection httpUrlConnection = null;
+    try {
+      httpUrlConnection = (HttpURLConnection) url.openConnection();
+    } catch (IOException e) {
+      LOG.error(e);
+    }
+    return httpUrlConnection;
+  }
 
+  public  boolean getContent(HttpURLConnection httpUrlConnection) {
+    if (httpUrlConnection != null) {
 
+      try {
+        BufferedInputStream bis = new BufferedInputStream(httpUrlConnection.getInputStream(), BUFFER_SIZE);
+        File weatherJsonDir = new File("./resources");
+        if (!weatherJsonDir.exists()) weatherJsonDir.mkdir();
+
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("./resources/weather-forecast.json"));
+        byte[] inputBuffer = new byte[BUFFER_SIZE];
+        int readBytes;
+        while((readBytes = bis.read(inputBuffer))>0){
+          bos.write(inputBuffer, 0, readBytes);
+        }
+        bis.close();
+        bos.close();
+
+        return true;
+      } catch (IOException e) {
+        LOG.error(e);
+        return false;
+      }
+    } else return false;
+  }
 }
