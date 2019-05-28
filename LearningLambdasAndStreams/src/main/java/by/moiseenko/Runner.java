16986466aa10d49c6df55aae5c37afa0a26894d1
slipknotbiocom_HOME <slipknotbiocom@gmail.com>
2019-05-28 20:56:05 +0300

    This update will add:
    1. Add new project for learning lambdas and streams.

---
+++ LearningLambdasAndStreams/src/main/java/by/moiseenko/Runner.java
@@ -0,0 +1,14 @@
+package by.moiseenko;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Default javadoc
+ *
+ * @author s-moiseenko
+ */
+public class Runner {
+  private static final Logger LOG = LogManager.getLogger(Runner.class.getName());
+
+}
