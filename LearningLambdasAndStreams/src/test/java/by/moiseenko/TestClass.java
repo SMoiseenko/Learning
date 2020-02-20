94677c6323630fba03db0a7df84f5ef107dc0103
slipknotbiocom_WORK <slipknotbiocom@gmail.com>
2020-02-20 16:51:02 +0300

    This update will add:
    1. Nothing new(

--- LearningLambdasAndStreams/src/test/java/by/moiseenko/TestClass.java
+++ LearningLambdasAndStreams/src/test/java/by/moiseenko/TestClass.java
@@ -3,7 +3,13 @@ package by.moiseenko;
 import by.moiseenko.entity.TheMan;
 import by.moiseenko.entity.TheManComparator;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
@@ -32,6 +38,7 @@ public class TestClass {
     mansList.sort(TheMan::compareTo);
     mansList.sort(new TheManComparator());
     mansList.forEach(LOG::debug);
+    mansList.removeIf(Objects::isNull);
     Assertions.assertEquals(1, 1);
   }
 
@@ -44,5 +51,12 @@ public class TestClass {
               })
           .start();
     }
+  }
+
+  @Test
+  void sortArray(){
+    Integer[] array = {20, 54, 2, 89, 65, 47, 356, 412, 589, 3, 79, 562};
+    Arrays.sort(array, Collections.reverseOrder());
+    LOG.debug(Arrays.toString(array));
   }
 }
