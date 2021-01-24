a9c950048c9e0827d463daf8eade2bfeaefa36c8
slipknotbiocom_HOME <slipknotbiocom@gmail.com>
2021-01-25 00:31:11 +0300

    25.01.2021
    Change to java 15 version

--- TempCustomerInfo/src/main/java/by/moiseenko/TempCustomerInfo/TempCustomerInfoApplication.java
+++ TempCustomerInfo/src/main/java/by/moiseenko/TempCustomerInfo/TempCustomerInfoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TempCustomerInfoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TempCustomerInfoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TempCustomerInfoApplication.class, args);
+  }
 
 }
