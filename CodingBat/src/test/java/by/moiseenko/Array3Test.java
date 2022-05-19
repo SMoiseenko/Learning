f250b756c2b349f716e1ccdbc232c52383ad6832
slipknotbiocom_WORK <slipknotbiocom@gmail.com>
2022-05-19 16:33:05 +0300

    19.05.2022
    Add tests for Array2 and Array3 classes.

--- CodingBat/src/test/java/by/moiseenko/Array3Test.java
+++ CodingBat/src/test/java/by/moiseenko/Array3Test.java
@@ -1,47 +1,75 @@
 package by.moiseenko;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import by.moiseenko.impl.Array3Impl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 /**
  * Change implementation in init() method to your.
  */
 class Array3Test {
 
-  @Test
-  void maxSpan() {
+  private static Array3 a3;
+
+  @BeforeAll
+  public static void init() {
+    a3 = new Array3Impl();
   }
 
-  @Test
-  void fix34() {
+  @ParameterizedTest
+  @MethodSource("by.moiseenko.dataprovider.Array3TestDataProvider#maxSpan")
+  void maxSpan(int[] nums, int expected) {
+    assertEquals(expected, a3.maxSpan(nums));
   }
 
-  @Test
-  void fix45() {
+  @ParameterizedTest
+  @MethodSource("by.moiseenko.dataprovider.Array3TestDataProvider#fix34")
+  void fix34(int[] nums, int[] expected) {
+    assertEquals(expected, a3.fix34(nums));
   }
 
-  @Test
-  void canBalance() {
+  @ParameterizedTest
+  @MethodSource("by.moiseenko.dataprovider.Array3TestDataProvider#fix45")
+  void fix45(int[] nums, int[] expected) {
+    assertEquals(expected, a3.fix45(nums));
   }
 
-  @Test
-  void linearIn() {
+  @ParameterizedTest
+  @MethodSource("by.moiseenko.dataprovider.Array3TestDataProvider#canBalance")
+  void canBalance(int[] nums, boolean expected) {
+    assertEquals(expected, a3.canBalance(nums));
   }
 
-  @Test
-  void squareUp() {
+  @ParameterizedTest
+  @MethodSource("by.moiseenko.dataprovider.Array3TestDataProvider#linearIn")
+  void linearIn(int[] outer, int[] inner, boolean expected) {
+    assertEquals(expected, a3.linearIn(outer, inner));
   }
 
-  @Test
-  void seriesUp() {
+  @ParameterizedTest
+  @MethodSource("by.moiseenko.dataprovider.Array3TestDataProvider#squareUp")
+  void squareUp(int n, int[] expected) {
+    assertEquals(expected, a3.squareUp(n));
   }
 
-  @Test
-  void maxMirror() {
+  @ParameterizedTest
+  @MethodSource("by.moiseenko.dataprovider.Array3TestDataProvider#seriesUp")
+  void seriesUp(int n, int[] expected) {
+    assertEquals(expected, a3.seriesUp(n));
   }
 
-  @Test
-  void countClumps() {
+  @ParameterizedTest
+  @MethodSource("by.moiseenko.dataprovider.Array3TestDataProvider#maxMirror")
+  void maxMirror(int[] nums, int expected) {
+    assertEquals(expected, a3.maxMirror(nums));
+  }
+
+  @ParameterizedTest
+  @MethodSource("by.moiseenko.dataprovider.Array3TestDataProvider#countClumps")
+  void countClumps(int[] nums, int expected) {
+    assertEquals(expected, a3.countClumps(nums));
   }
 }
